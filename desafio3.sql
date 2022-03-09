6969af81237272e34d889ba4f0d4f87b637744e4
Mariana Saraiva <marianascm8@gmail.com>
2022-03-09 08:53:43 -0300

    feat: requirement 3

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+SELECT
+  usuario AS usuario,
+  COUNT(HR.cancoes_id) AS `qtde_musicas_ouvidas`,
+  ROUND(SUM(C.duracao_segundos)/60, 2) AS `total_minutos`
+FROM SpotifyClone.Usuario AS U
+JOIN SpotifyClone.HistoricoReproducao AS HR
+ON U.usuario_id = HR.usuario_id
+JOIN SpotifyClone.Cancoes AS C
+ON HR.cancoes_id = C.cancoes_id
+GROUP BY U.usuario
+ORDER BY U.usuario;
